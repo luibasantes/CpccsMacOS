1092ede0d9b3c1e557cd3cbc0aa7145ea7c66453
luibasantes <luibasantes@hotmail.com>
2017-08-24 16:11:48 -0500

    Creando la ventana principal desde 0

---
+++ appCpccs/appCpccs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appCpccs
+//
+//  Created by Estudiante on 24/8/2017.
+//  Copyright (c) 2017 Espol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
