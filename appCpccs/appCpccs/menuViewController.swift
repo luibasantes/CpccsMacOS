119e37b499e18ae40c938b92396d7d2def5acade
luibasantes <luibasantes@hotmail.com>
2017-09-06 15:17:35 -0500

    agregando ventana de submenu funcionalidades

---
+++ appCpccs/appCpccs/menuViewController.swift
@@ -0,0 +1,23 @@
+//
+//  menuViewController.swift
+//  appCpccs
+//
+//  Created by Estudiante on 6/9/2017.
+//  Copyright (c) 2017 Espol. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class menuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
