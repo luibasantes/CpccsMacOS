b9217713b1c10ac3a29207d696c47b5c338b465e
Sistemas Operativos <SO@iMac27-C201.local>
2017-09-07 15:58:42 -0500

    Creacion de funciones para llamar views al container (no implementado totalemente)

---
+++ appCpccs/appCpccs/ControlSocialController.swift
@@ -0,0 +1,9 @@
+//
+//  ControlSocialController.swift
+//  appCpccs
+//
+//  Created by Sistemas Operativos on 7/9/17.
+//  Copyright (c) 2017 Espol. All rights reserved.
+//
+
+import Foundation
---
+++ appCpccs/appCpccs/ParticipacionController.swift
@@ -0,0 +1,9 @@
+//
+//  ParticipacionController.swift
+//  appCpccs
+//
+//  Created by Sistemas Operativos on 7/9/17.
+//  Copyright (c) 2017 Espol. All rights reserved.
+//
+
+import Foundation
